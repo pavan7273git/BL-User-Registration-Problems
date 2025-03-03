39ec7873c3d7a31bccbee0b29ec7ff18ada9b99b
pavan <pavankumarreddy155@gmail.com>
2025-03-03 11:29:12 +0530

    [Pavan] Add: Python script for checking entered first name is valid or not

--- user_registration.py
+++ user_registration.py
@@ -1,1 +1,13 @@
-print('welcome to user registration')
+import re
+
+def is_valid_first_name(name):
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'  
+    return bool(re.match(pattern, name))
+
+while True:
+    first_name = input("Enter your First Name: ")
+    if is_valid_first_name(first_name):
+        print(f"{first_name} is a Valid First Name.")
+        break
+    else:
+        print("Invalid First Name! It should start with a capital letter and have at least 3 characters.")
