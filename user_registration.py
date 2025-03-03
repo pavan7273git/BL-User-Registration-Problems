25c523f37de4c8f388a2e931854514268c99013a
pavan <pavankumarreddy155@gmail.com>
2025-03-03 11:37:38 +0530

    [Pavan] Add: Python script for checking the entered last name is valid or not

--- user_registration.py
+++ user_registration.py
@@ -1,13 +1,21 @@
 import re
 
-def is_valid_first_name(name):
+def is_valid_name(name):
     pattern = r'^[A-Z][a-zA-Z]{2,}$'  
     return bool(re.match(pattern, name))
 
 while True:
     first_name = input("Enter your First Name: ")
-    if is_valid_first_name(first_name):
+    if is_valid_name(first_name):
         print(f"{first_name} is a Valid First Name.")
         break
     else:
         print("Invalid First Name! It should start with a capital letter and have at least 3 characters.")
+
+while True:
+    last_name = input("Enter your Last Name: ")
+    if is_valid_name(last_name):
+        print(f"{last_name} is a Valid Last Name.")
+        break
+    else:
+        print("Invalid Last Name! It should start with a capital letter and have at least 3 characters.")
