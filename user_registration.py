bfc1304b6fd5bb504fc5b9edb049bb6512a93758
pavan <pavankumarreddy155@gmail.com>
2025-03-03 11:45:10 +0530

    [Pavan] Add: Python script for checking email is valid or not

--- user_registration.py
+++ user_registration.py
@@ -4,6 +4,10 @@ def is_valid_name(name):
     pattern = r'^[A-Z][a-zA-Z]{2,}$'  
     return bool(re.match(pattern, name))
 
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$'
+    return bool(re.match(pattern, email))
+
 while True:
     first_name = input("Enter your First Name: ")
     if is_valid_name(first_name):
@@ -19,3 +23,12 @@ while True:
         break
     else:
         print("Invalid Last Name! It should start with a capital letter and have at least 3 characters.")
+
+while True:
+    email = input("Enter an email address: ")
+    if is_valid_email(email):
+        print(f"{email} is a Valid Email Address.")
+        break
+    else:
+        print("Invalid Email Address! Example: abc.xyz@bl.co.in")
+
