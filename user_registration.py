2877b07080348d768f27c39d253a44706943a1b5
pavan <pavankumarreddy155@gmail.com>
2025-03-04 11:06:29 +0530

    [Pavan] Add: Validating the password it should contains atleast 8 characters

--- user_registration.py
+++ user_registration.py
@@ -1,16 +1,30 @@
 import re
 
+'''As a User need to
+follow pre
+-defined
+Password rules.
+Rule1
+minimum 8
+Characters - NOTE All rules must be passed'''
+
+
+
 def is_valid_name(name):
-    pattern = r'^[A-Z][a-zA-Z]{2,}$'  
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'  #it checks the first letter is capital and minimum of 3 characters
     return bool(re.match(pattern, name))
 
-def is_valid_email(email):
+def is_valid_email(email):  #it checks the mail have the @ and . in mail
     pattern = r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$'
     return bool(re.match(pattern, email))
 
 def is_valid_mobile(mobile):
-    pattern = r'^[0-9]{2} [0-9]{10}$'
+    pattern = r'^[0-9]{2} [0-9]{10}$'  #it checks the numbers are 10
     return bool(re.match(pattern, mobile))
+
+def is_valid_password(password):
+    pattern = r'^.{8,}$'  # it checks the password contain  atleast 8 characters
+    return bool(re.match(pattern, password))
 
 while True:
     first_name = input("Enter your First Name: ")
@@ -34,12 +48,20 @@ while True:
         print(f"{email} is a Valid Email Address.")
         break
     else:
-        print("Invalid Email Address. Reenter the Email Address")
+        print("Invalid Email Address! Example: abc.xyz@bl.co.in")
 
 while True:
-    mobile = input("Enter Mobile Number ( CountryCode Space 10-Digit Number): ")
+    mobile = input("Enter Mobile Number (CountryCode Space 10-Digit Number): ")
     if is_valid_mobile(mobile):
         print(f"{mobile} is a Valid Mobile Number.")
         break
     else:
-        print("Invalid Mobile Number. Reenter the number it should be 10 digit number")
+        print("Invalid Mobile Number! Example: 91 9919819801 (Country code followed by space and 10-digit number).")
+
+while True:
+    password = input("Enter Password (Minimum 8 characters): ")
+    if is_valid_password(password):
+        print("Valid Password.")
+        break
+    else:
+        print("Invalid Password! Password must be at least 8 characters long.")
